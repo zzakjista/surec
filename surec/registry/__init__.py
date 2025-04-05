92fd638926aa97564900feedf7e73e9e4c5f2793
zzakjista <gnsdl09@kookmin.ac.kr>
2025-04-05 16:18:14 +0900

    feat: add draft model registry

--- surec/registry/__init__.py
+++ surec/registry/__init__.py
@@ -2,3 +2,5 @@
 # Distributed under the terms of the Apache License 2.0.
 # For more details, visit: https://www.apache.org/licenses/LICENSE-2.0
 
+from surec.registry.model_registry import ModelRegistry
+model_registry = ModelRegistry()
